76b591ed17b54b1436c9575a9872555468d862fc
Andrew Lee <candrewlee14@gmail.com>
2022-04-07 17:13:34 -0600

    add global API for default logger

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,13 @@
 const std = @import("std");
 const zlog = @import("zlog.zig");
-const log = @import("zlog_log.zig");
+const logger = &@import("zlog_log.zig").logger;
 
 pub fn main() anyerror!void {
-    var logger2 = try log.globalLogMan.Logger(@TypeOf(log.globalWriter), .pretty, .debug)
-        .new(log.globalWriter);
-    try logger2.print("hey there");
-    var sublogger = try logger2.level(.warn);
+    try logger.print("hey there");
+    var sublogger = try logger.level(.warn);
     try sublogger.print("look, a warning!");
 
-    var event = try logger2.withLevel(.err);
+    var event = try logger.withLevel(.err);
     try event.str("Hey2", "This is also a field");
     try event.num("Value1", 10);
     try event.msgf("This is using msgf to append a {s} here and {d} here", 
--- src/zlog_log.zig
+++ src/zlog_log.zig
@@ -1,8 +1,39 @@
 const std = @import("std");
 const zlog = @import("zlog.zig");
 
-pub const globalLogConf = zlog.LogConfig.default();
-pub const globalLogMan = zlog.LogManager(globalLogConf);
-pub const globalWriter = std.io.getStdErr().writer();
-pub var logger = globalLogMan.Logger(@TypeOf(globalWriter), .json, .debug)
-    .new(globalWriter) catch @panic("Failed to create global logger");
+pub const logConf = zlog.LogConfig.default();
+pub const logMan = zlog.LogManager(logConf);
+pub const writer = std.io.getStdErr().writer();
+pub const fmtMode = .json;
+pub const loggerDefaultLevel = .debug;
+pub var logger = logMan.Logger(@TypeOf(writer), fmtMode, loggerDefaultLevel)
+    .new(writer) catch @panic("Failed to create global logger");
+
+/// Return current logger log level
+pub fn getLevel() zlog.LevelType {
+    return logger.getLevel();
+}
+/// Returns a sublogger at the given log level
+pub fn level( comptime lvl: zlog.LevelType,) !zlog.Logger(@TypeOf(writer), fmtMode, loggerDefaultLevel) {
+    return logger.level(lvl);
+}
+/// Add key:boolean context to this logger
+pub fn booleanCtx(key: []const u8, val: bool) !void {
+    try logger.booleanCtx(key, val);
+}
+/// Add key:num context to this logger
+pub fn numCtx(key: []const u8, val: anytype) !void {
+    try logger.numCtx(key, val);
+}
+/// Add key:string context to this logger
+pub fn strCtx(key: []const u8, val: []const u8) !void {
+    try logger.strCtx(key, val);
+}
+/// Returns an event that at the given log level 
+pub fn withLevel( comptime lvl: zlog.LevelType) !zlog.Event(@TypeOf(writer), fmtMode, loggerDefaultLevel) {
+    return logger.withLevel(lvl);
+}
+/// Log a message at this logger's level
+pub fn print(msg: []const u8) !void {
+    try logger.print(msg);
+}
